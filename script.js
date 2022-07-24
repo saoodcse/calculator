dbc9eeb1d92aec00a0f0bfb59e32cb99794d8b09
saoodcse <109861858+saoodcse@users.noreply.github.com>
2022-07-24 15:27:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const input = document.querySelector("#input");
+
+const grid = document.querySelector(".grid-4-col");
+let str = "";
+
+grid.addEventListener("click", function (e) {
+  input.style.backgroundColor = "black";
+  if (e.target.classList.contains("btn")) {
+    console.log(e.target);
+    let buttonValues = e.target.innerHTML;
+    switch (buttonValues) {
+      case "c":
+        str = "";
+        input.value = str;
+        break;
+      case "←":
+        console.log(buttonValues);
+        str = str.slice(0, -1);
+        input.value = str;
+        break;
+      case "=":
+        try {
+          let result = eval(str);
+          if (Number.isInteger(result)) {
+            str = result;
+            input.value = result;
+          } else {
+            str = parseFloat(result).toFixed(2);
+            input.value = str;
+          }
+        } catch (error) {
+          input.style.backgroundColor = "red";
+          // console.log
+        }
+
+        break;
+
+      default:
+        if (str === "0") {
+          str = buttonValues;
+          input.value = str;
+        } else {
+          str += buttonValues;
+          input.value = str;
+          break;
+        }
+    }
+  }
+});
